50d275edfdb6390e3aa97376c0bfb8e6e6605a8f
Rdkang <athik@outlook.com>
2023-12-01 18:28:24 +0000

    feat: testing new changelog file testo

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ fn main() {
     println!("Hello, Rdkang!");
     println!("I have fully automated the relase progress :)");
     println!("scrap that, the cliff notes are not only for the current tag relase");
+    println!("might have realised that it's using the changelog.md file not new generated one");
 }
