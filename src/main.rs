c06a41b0ae7a3b6036162ff3abd915b13bf3b552
Rdkang <athik@outlook.com>
2023-12-01 17:13:45 +0000

    feat: update to current progress of changelog

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     println!("Hello, world!");
     println!("Hello, Rdkang!");
     println!("I have fully automated the relase progress :)");
+    println!("scrap that, the cliff notes are not only for the current tag relase");
 }
